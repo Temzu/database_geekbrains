190c975b6fb9ee15ff6b0ec427a184d59b345df3
Temzu <40913z@mail.ru>
2020-12-08 20:24:49 +0300

    completed_task

---
+++ Lesson_2/hw.sql
@@ -0,0 +1,126 @@
+ALTER TABLE `geodata`.`_countries` 
+DROP COLUMN `title_cz`,
+DROP COLUMN `title_lv`,
+DROP COLUMN `title_lt`,
+DROP COLUMN `title_ja`,
+DROP COLUMN `title_pl`,
+DROP COLUMN `title_it`,
+DROP COLUMN `title_fr`,
+DROP COLUMN `title_de`,
+DROP COLUMN `title_pt`,
+DROP COLUMN `title_es`,
+DROP COLUMN `title_en`,
+DROP COLUMN `title_be`,
+DROP COLUMN `title_ua`,
+CHANGE COLUMN `country_id` `id` INT NOT NULL AUTO_INCREMENT ,
+CHANGE COLUMN `title_ru` `title` VARCHAR(150) NOT NULL ,
+ADD PRIMARY KEY (`id`),
+ADD INDEX `idx_title` (`title_ru` ASC) VISIBLE;
+
+ALTER TABLE `geodata`.`_regions` 
+DROP COLUMN `title_cz`,
+DROP COLUMN `title_lv`,
+DROP COLUMN `title_lt`,
+DROP COLUMN `title_ja`,
+DROP COLUMN `title_pl`,
+DROP COLUMN `title_it`,
+DROP COLUMN `title_fr`,
+DROP COLUMN `title_de`,
+DROP COLUMN `title_pt`,
+DROP COLUMN `title_es`,
+DROP COLUMN `title_en`,
+DROP COLUMN `title_be`,
+DROP COLUMN `title_ua`,
+CHANGE COLUMN `region_id` `id` INT NOT NULL AUTO_INCREMENT ,
+CHANGE COLUMN `title_ru` `title` VARCHAR(150) NOT NULL ,
+ADD PRIMARY KEY (`id`);
+;
+
+ALTER TABLE `geodata`.`_regions` 
+ADD INDEX `idx_title` (`title` ASC) VISIBLE;
+;
+
+ALTER TABLE `geodata`.`_regions` 
+ADD INDEX `fk_regions_country_idx` (`country_id` ASC) VISIBLE;
+;
+
+ALTER TABLE `geodata`.`_regions` 
+ADD CONSTRAINT `fk_regions_country`
+  FOREIGN KEY (`country_id`)
+  REFERENCES `geodata`.`_countries` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
+
+
+SET GLOBAL innodb_buffer_pool_size=2147483648;
+SHOW STATUS WHERE Variable_name='InnoDB_buffer_pool_resize_status';
+
+use geodata;
+delete from _cities where region_id is null;
+
+ALTER TABLE `geodata`.`_cities` 
+DROP COLUMN `region_cz`,
+DROP COLUMN `area_cz`,
+DROP COLUMN `title_cz`,
+DROP COLUMN `region_lv`,
+DROP COLUMN `area_lv`,
+DROP COLUMN `title_lv`,
+DROP COLUMN `region_lt`,
+DROP COLUMN `area_lt`,
+DROP COLUMN `title_lt`,
+DROP COLUMN `region_ja`,
+DROP COLUMN `area_ja`,
+DROP COLUMN `title_ja`,
+DROP COLUMN `region_pl`,
+DROP COLUMN `area_pl`,
+DROP COLUMN `title_pl`,
+DROP COLUMN `region_it`,
+DROP COLUMN `area_it`,
+DROP COLUMN `title_it`,
+DROP COLUMN `region_fr`,
+DROP COLUMN `area_fr`,
+DROP COLUMN `title_fr`,
+DROP COLUMN `region_de`,
+DROP COLUMN `area_de`,
+DROP COLUMN `title_de`,
+DROP COLUMN `region_pt`,
+DROP COLUMN `area_pt`,
+DROP COLUMN `title_pt`,
+DROP COLUMN `region_es`,
+DROP COLUMN `area_es`,
+DROP COLUMN `title_es`,
+DROP COLUMN `region_en`,
+DROP COLUMN `area_en`,
+DROP COLUMN `title_en`,
+DROP COLUMN `region_be`,
+DROP COLUMN `area_be`,
+DROP COLUMN `title_be`,
+DROP COLUMN `region_ua`,
+DROP COLUMN `area_ua`,
+DROP COLUMN `title_ua`,
+DROP COLUMN `region_ru`,
+DROP COLUMN `area_ru`,
+CHANGE COLUMN `city_id` `id` INT NOT NULL AUTO_INCREMENT ,
+CHANGE COLUMN `region_id` `region_id` INT NOT NULL ,
+CHANGE COLUMN `title_ru` `title` VARCHAR(150) NOT NULL ,
+ADD PRIMARY KEY (`id`),
+ADD INDEX `idx_title` (`title` ASC) VISIBLE,
+ADD INDEX `fk_cities_countries_idx` (`country_id` ASC) VISIBLE,
+ADD INDEX `fk_cities_regions_idx` (`region_id` ASC) VISIBLE;
+;
+
+ALTER TABLE `geodata`.`_cities` 
+ADD CONSTRAINT `fk_cities_countries`
+  FOREIGN KEY (`country_id`)
+  REFERENCES `geodata`.`_countries` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT,
+ADD CONSTRAINT `fk_cities_regions`
+  FOREIGN KEY (`region_id`)
+  REFERENCES `geodata`.`_regions` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
+
+
+
+
